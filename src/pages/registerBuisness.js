6351551a99c17306c871014affa7893d005d892d
zohar-tzabari <80604271+zohar-tzabari@users.noreply.github.com>
2023-04-29 22:15:36 +0300

    Update registerBuisness.js

--- src/pages/registerBuisness.js
+++ src/pages/registerBuisness.js
@@ -1,72 +1,106 @@
 import { Form, Input, Button, Layout, message, Upload } from "antd";
 import { ClientRegisterApi } from "../api/usersApi";
-import { InboxOutlined } from '@ant-design/icons';
-import { MINIAPPUSER } from "../CONST";
+import { InboxOutlined } from "@ant-design/icons";
 import { useState } from "react";
 
-
 const { Header, Content, Footer, Sider } = Layout;
+const { Dragger } = Upload;
 
-const UploadFile = ()=>{
-    const [messageApi, contextHolder] = message.useMessage();
+const UploadFile = () => {
+  const [messageApi, contextHolder] = message.useMessage();
+  const [buisnessImg, setBuisnesImg] = useState(null);
 
-    const successMsg = (text) => {
-        messageApi.open({
-          type: "success",
-          content: text,
-        });
-      };
-    
-      const errorMsg = (text) => {
-        messageApi.open({
-          type: "error",
-          content: text,
-        });
-      };
-    const { Dragger } = Upload;
+  const successMsg = (text) => {
+    messageApi.open({
+      type: "success",
+      content: text,
+    });
+  };
 
-const props = {
-     
-    name: 'file',
+  const errorMsg = (text) => {
+    messageApi.open({
+      type: "error",
+      content: text,
+    });
+  };
+
+  const sendFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      setBuisnesImg(reader.result);
+    };
+  };
+
+
+
+  const props = {
+    name: "file",
     multiple: false,
-    action: console.log('hi') ,
-    accept: '.png',
+    action: "",
+    accept: ".png",
+    className:"avatar-uploader",
+    disabled: buisnessImg !== null,
+    onRemove(info){
+      setBuisnesImg(null);
+    },
     onChange(info) {
-        const { status, response } = info.file;
-        if (status !== 'uploading') {
-            console.log(info.file, info.fileList);
-        }
-        if (status === 'done') {
-            console.log(info.file);
-            // setUploadData(true);;
-            message.successMsg(`${info.file.name} file uploaded successfully.`);
-            // setAllFiles(allFiles.push(info.file));
-            console.log(info.file);
-        }
-        else if (status === 'error') {
-            message.errorMsg(`${info.file.name} file upload failed.`);
-        }
-    }
-};
-return(
-  <Dragger {...props}>
-    <p className="ant-upload-drag-icon">
-      <InboxOutlined />
-    </p>
-    <p className="ant-upload-text">Click or drag file to this area to upload</p>
-    <p className="ant-upload-hint">
-      Support for a single or bulk upload. Strictly prohibited from uploading company data or other
-      banned files.
-    </p>
-  </Dragger>
-);
-}
+      const { status, response } = info.file;
+      if (status !== "uploading") {
+        console.log(info.file, info.fileList);
+      }
+      if (status === "done") {
+        console.log(info.file);
+        successMsg(`${info.file.name} file uploaded successfully.`);
+      } else if (status === "error") {
+        errorMsg(`${info.file.name} file upload failed.`);
+      }
+    },
+    customRequest: ({ onSuccess, onError, file }) => {
+      setTimeout(() => {
+        onSuccess("ok");
+        sendFile(file);
+      }, 0);
+    },
+  };
 
+  return (
+    <div>
+      <Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">
+          Click or drag file to this area to upload
+        </p>
+        <p className="ant-upload-hint">
+          Support for a single or bulk upload. Strictly prohibited from
+          uploading company data or other banned files.
+        </p>
+      </Dragger>
+      {buisnessImg && (
+        <div>
+          <img
+            src={buisnessImg}
+            alt="Selected Business Image"
+            style={{
+              height: "20vh",
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+          />
+        </div>
+      )}
+    </div>
+  );
+};
 
 
 const RegistrationFormContent = () => {
   const [messageApi, contextHolder] = message.useMessage();
-  const [role, setRole] = useState(MINIAPPUSER);
+  // const [role, setRole] = useState(MINIAPPUSER);
 
   const successMsg = (text) => {
     messageApi.open({
@@ -132,7 +166,7 @@ const RegistrationFormContent = () => {
           </Button>
         </Form.Item>
       </Form>
-      <UploadFile/>
+      <UploadFile />
       //TODO dropdown of supplier type
     </>
   );
