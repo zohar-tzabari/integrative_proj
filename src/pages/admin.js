eca83eda61ca1c40762bcbe46f47e51d9066700e
zohar-tzabari <80604271+zohar-tzabari@users.noreply.github.com>
2023-05-01 23:19:07 +0300

    fix admin table

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -10,20 +10,44 @@ import { useEffect, useState } from "react";
 
 const { Header, Content, Footer, Sider } = Layout;
 
+
+
 const JsonTable = ({ data }) => {
   const [columns, setColumns] = useState([]);
 
   useEffect(() => {
     if (data.length > 0) {
       const keys = Object.keys(data[0]);
-      const newColumns = keys.map((key) => ({
-        title: key.toUpperCase(),
-        dataIndex: key,
-        key,
-      }));
+      const newColumns = keys.map((key) => {
+        const dataIndex = key;
+        const title = key.toUpperCase();
+        const value = data[0][key];
+        if (typeof value === "object" && value !== null) {
+          const subKeys = Object.keys(value);
+          const children = subKeys.map((subKey) => ({
+            title: subKey.toUpperCase(),
+            dataIndex: `${key}.${subKey}`,
+            key: `${key}.${subKey}`,
+            render: (text, record) => record[key][subKey],
+          }));
+          return {
+            title,
+            dataIndex,
+            key,
+            children,
+          };
+        }
+        return {
+          title,
+          dataIndex,
+          key,
+        };
+      });
       setColumns(newColumns);
     }
   }, [data]);
+
+
 
   return <Table dataSource={data} columns={columns} />;
 };
