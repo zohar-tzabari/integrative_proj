9f778c62fda55bc60e8702fdde7ed526f0745987
liri9 <lichow.liri@gmail.com>
2023-05-28 20:41:32 +0300

    Update commandApi.js

--- src/api/commandApi.js
+++ src/api/commandApi.js
@@ -1,45 +1,13 @@
 import axios from "axios";
 import { useSelector } from "react-redux";
 
-<<<<<<< HEAD
-// export async function GetAllSuppliers(miniAppName) {
-//   console.log(values);
-//   let dataToSend = {};
-//   dataToSend["command"] = "getAllSuppliers";
-//   try {
-//     const response = await axios.post(
-//       `http://localhost:8081/superapp/miniapp/${miniAppName}`,
-//       dataToSend,
-//       {
-//         headers: {
-//           "Content-Type": "application/json",
-//           Accept: "application/json",
-//         },
-//       }
-//     );
-//     return response.data;
-//   } catch (error) {
-//     console.error(error);
-//     return null;
-//   }
-// }
 
-export async function searchObjectsByType() {
-  //console.log(values);
+export async function searchObjectsByType(email) {
   let dataToSend = {};
   dataToSend["command"] = "searchObjectsByType";
   try {
     const response = await axios.get(
-      `http://localhost:8081/superapp/objects/search/byType/Supplier?userEmail=liriella71@gmail.com`,
-=======
-export async function GetAllSuppliers(values) {
-  console.log(values);
-  let dataToSend = {};
-  const miniApp = useSelector((state) => state.miniApp);
-  dataToSend["command"] = "getAllSuppliers";
-  try {
-    const response = await axios.post(
-      `http://localhost:8081/superapp/miniapp/${miniApp.miniAppName}`,
+      `http://localhost:8081/superapp/objects/search/byType/Supplier?userEmail=${email}`,
       dataToSend,
       {
         headers: {
@@ -55,6 +23,7 @@ export async function GetAllSuppliers(values) {
   }
 }
 
+
 export async function GetAllGuests(email) {
   const miniApp = useSelector((state) => state.miniApp);
   const objectM = useSelector((state) => state.objectManager);
@@ -69,7 +38,6 @@ export async function GetAllGuests(email) {
   try {
     const response = await axios.post(
       `http://localhost:8081/superapp/miniapp/${miniApp.miniAppName}`,
->>>>>>> master
       dataToSend,
       {
         headers: {
