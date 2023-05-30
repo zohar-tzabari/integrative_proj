33df95ec7d57ddad67f4852bad097f7fa08574d3
zohar-tzabari <80604271+zohar-tzabari@users.noreply.github.com>
2023-05-30 11:22:14 +0300

    [objecta api] add by type and by alias

--- src/api/objectsApi.js
+++ src/api/objectsApi.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export async function CreateNewObject(values) {
-  console.log(values);
   try {
     const response = await axios.post(
       `http://localhost:8081/superapp/objects`,
@@ -20,3 +19,39 @@ export async function CreateNewObject(values) {
   }
 }
 
+export async function GetObjectByType(type,userEmail) {
+  try {
+    const response = await axios.get(
+      `http://localhost:8081/superapp/objects/search/byType/${type}`,
+      {
+        params: {
+          userSuperapp: "2023b.zohar.tzabari",
+          userEmail: userEmail,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+
+export async function GetObjectByAlias(alias,userEmail) {
+  try {
+    const response = await axios.get(
+      `http://localhost:8081/superapp/objects/search/byAlias/${alias}`,
+      {
+        params: {
+          userSuperapp: "2023b.zohar.tzabari",
+          userEmail: userEmail,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
