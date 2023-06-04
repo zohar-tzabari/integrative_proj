dfa97c76689402b37a3e1618d10423450ca67453
zohar-tzabari <80604271+zohar-tzabari@users.noreply.github.com>
2023-06-04 12:56:09 +0300

    [style] change colors

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
       locale={enUS}
       theme={{
         components: {
-          Layout: {
-            padding: 0,
-            background: "rgba(255, 255, 100, 0.1)",
-            backgroundImage:
-              'url("https://img.freepik.com/free-vector/light-pink-heart-pattern_53876-67660.jpg")',
-            backgroundSize: "20%",
-            backgroundRepeat: "repeat",
+          Button: {
+            colorPrimary: "#820034",
+            colorPrimaryActive	:"#4f0619",
+            colorPrimaryHover:"#f0c0cd"
+          },
+          Steps: {
+            colorPrimary: "#820034",
           },
         },
       }}
